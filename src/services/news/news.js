f9324feee8c2b8e13adc74d6918550e27ec3fae1
Robert Morigi-Eades <robe16@users.noreply.github.com>
2018-05-12 16:32:40 +0100

    Updates to align to jarvis.news service

--- src/services/news/news.js
+++ src/services/news/news.js
@@ -26,46 +26,46 @@ function startNews(pageLocation, serviceIP) {
 		// Parse json
 		dataJson = JSON.parse(data);
 		//
-		var news = dataJson.news_articles;
+		var news_articles = dataJson.articles;
+		var news_sources = dataJson.sources;
 		//
 		var tempArticles = new Array();
 		//
-		for (var s in news) {
+//		for (var s in news) {
+//			var source = news[s];
+//			var sourceArticles = source.articles;
+//			var sourceDetails = source.source_details;
+//		}
+		//
+		for (var a in news_articles) {
+			var article = news_articles[a];
 			//
-			var source = news[s];
-			var sourceArticles = source.articles;
-			var sourceDetails = source.source_details;
+		    var articleDateObj = moment(article.publishedAt, "YYYY-MM-DDTh:mm:ssZ"); //2017-02-23T11:48:47Z
+		    var articleDatetime = articleDateObj.format("DD-MM-YYYY HH:mm");
+		    //
+		    var headline = article.title;
+		    //
+			// 'urlsToLogos' no longer available from newsapi.org
+//			var imgSource = document.createElement("IMG");
+//			imgSource.src = article.urlToImage;
+//			imgSource.className = "grayscale news_img";
+//			var divImgSource = document.createElement("DIV");
+//			divImgSource.className = "col-xs-1";
+//			divImgSource.appendChild(imgSource)
+			var divTitleArticle = document.createElement("DIV");
+			divTitleArticle.className = "col-xs-9 news_title"; //col-xs-8 when logo present
+			divTitleArticle.innerHTML = headline;
+			var divTitleDatetime = document.createElement("DIV");
+			divTitleDatetime.className = "col-xs-3 material-text-light-disabled news_date";
+			divTitleDatetime.innerHTML = articleDatetime;
 			//
-			for (var a in sourceArticles) {
-				var article = sourceArticles[a];
-				//
-			    var articleDateObj = moment(article.publishedAt, "YYYY-MM-DDTh:mm:ssZ"); //2017-02-23T11:48:47Z
-			    var articleDatetime = articleDateObj.format("DD-MM-YYYY HH:mm");
-			    //
-			    var headline = article.title;
-			    //
-				var imgSource = document.createElement("IMG");
-				imgSource.src = sourceDetails.logos.small;
-				imgSource.className = "grayscale news_img";
-				var divImgSource = document.createElement("DIV");
-				divImgSource.className = "col-xs-1";
-				divImgSource.appendChild(imgSource)
-				var divTitleArticle = document.createElement("DIV");
-				divTitleArticle.className = "col-xs-8 news_title";
-				divTitleArticle.innerHTML = headline;
-				var divTitleDatetime = document.createElement("DIV");
-				divTitleDatetime.className = "col-xs-3 material-text-light-disabled news_date";
-				divTitleDatetime.innerHTML = articleDatetime;
-				//
-				var rowDiv = document.createElement("DIV");
-				rowDiv.className = "row article_row";
-				rowDiv.appendChild(divImgSource);
-				rowDiv.appendChild(divTitleArticle);
-				rowDiv.appendChild(divTitleDatetime);
-				//
-				tempArticles[articleDateObj.format("YYYY-MM-DD HH:mm")] = rowDiv;
-				//
-			}
+			var rowDiv = document.createElement("DIV");
+			rowDiv.className = "row article_row";
+//			rowDiv.appendChild(divImgSource);
+			rowDiv.appendChild(divTitleArticle);
+			rowDiv.appendChild(divTitleDatetime);
+			//
+			tempArticles[articleDateObj.format("YYYY-MM-DD HH:mm")] = rowDiv;
 			//
 		}
 		//

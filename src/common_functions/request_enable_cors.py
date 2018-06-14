5b562493a25c873693d32c6c641811b71771b35c
Rob <robe16@hotmail.co.uk>
2018-06-14 23:31:32 +0100

    Update to PEP compliance

--- src/common_functions/request_enable_cors.py
+++ src/common_functions/request_enable_cors.py
@@ -1,5 +1,6 @@
 from resources.global_resources.variables import service_header_clientid_label
 
+
 def enable_cors(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET'

06f246067fba87d7aa13755e8410c8a06ed65b1e
Rob <robe16@hotmail.co.uk>
2018-06-14 19:17:33 +0100

    Update to remove exposed_apis.py and tidy of imported libraries/functions no longer in use.

--- src/portlistener.py
+++ src/portlistener.py
@@ -1,19 +1,11 @@
 import threading
-import os
-from datetime import datetime
 
-from bottle import HTTPError
-from bottle import get, post
-from bottle import request, run, HTTPResponse, static_file
+from bottle import get
+from bottle import request, run
 
-from common_functions.query_to_string import convert_query_to_string
 from config.config import get_cfg_port_listener
-from config.config import get_cfg_serviceid, get_cfg_name_long, get_cfg_name_short, get_cfg_groups, get_cfg_subservices
-from config.config import get_cfg_details_modules
-from log.log import log_inbound, log_internal, log_outbound
-from resources.global_resources.exposed_apis import *
-from resources.global_resources.log_vars import logPass, logFail, logException
-from resources.global_resources.variables import *
+from log.log import log_internal
+from resources.global_resources.log_vars import logPass
 from resources.lang.enGB.logs import *
 
 from apis.uri_config import get_config
@@ -35,23 +27,23 @@ def start_bottle(port_threads):
     # APIs
     ################################################################################################
 
-    @get(uri_config)
+    @get('/config')
     def api_get_config():
         return get_config(request)
 
-    @get(uri_get_ui_index)
+    @get('/')
     def api_get_ui_index():
         return get_ui_index(request)
 
-    @get(uri_get_ui_config)
+    @get('/config/config.js')
     def api_get_ui_config():
         return get_ui_config(request)
 
-    @get(uri_get_ui_resource)
+    @get('/static/<type>/<filename>')
     def api_get_ui_resource(type, filename):
         return get_ui_resource(request, type, filename)
 
-    @get(uri_get_ui_module)
+    @get('/services/<service>/<filename>')
     def api_get_ui_module(service, filename):
         return get_ui_module(request, service, filename)
 

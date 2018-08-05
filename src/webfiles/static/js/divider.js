a1a5104cb82730cf4334527b61299f4f97d18115
Rob <robe16@hotmail.co.uk>
2018-08-05 17:42:13 +0100

    Updated to improve divider appearance

--- src/webfiles/static/js/divider.js
+++ src/webfiles/static/js/divider.js
@@ -1,8 +1,5 @@
 function createDivider(moduleId) {
     var divider = document.createElement("HR");
     divider.className = "divider material-text-light-secondary";
-    var dividerDiv = document.createElement("DIV");
-    dividerDiv.className = "row";
-    dividerDiv.appendChild(divider);
-    return dividerDiv;
+    return divider;
 }
